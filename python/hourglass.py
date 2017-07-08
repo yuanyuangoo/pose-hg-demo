cb4690b4eae050ddc52cf75d9fe1f164d176a157
yuanyuangoo <yuanyuangoo@hotmail.com>
2017-07-09 10:44:02 +1200

    0709

--- python/hourglass.py
+++ python/hourglass.py
@@ -1,6 +1,5 @@
 import PyTorch
 import PyTorchHelpers
-<<<<<<< HEAD
 import numpy
 def run(im_names,box):
     TorchModel = PyTorchHelpers.load_lua_class('pose-hg-demo/python/TorchModel.lua', 'TorchModel')
@@ -9,13 +8,3 @@ def run(im_names,box):
 #    disp=PyTorch.DoubleTensor(2,3,378,756)
     result = TorchModel.predict(im_names,box)
     return result.asNumpyTensor()
-
-=======
-def run(im_names,box):
-    TorchModel = PyTorchHelpers.load_lua_class('pose-hg-demo/python/TorchModel.lua', 'TorchModel')
-    TorchModel=TorchModel()
-    result=TorchModel.predict(im_names,box)
-
-    print('adfafadfadfa')
-    print(result)
->>>>>>> 80ad5db85ee28361092966dafee7930052d89659
